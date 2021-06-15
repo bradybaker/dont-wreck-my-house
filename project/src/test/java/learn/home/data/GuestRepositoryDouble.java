a9b2dd208467994873f084145f556d86069def37
Brady Baker <bradybaker82@gmail.com>
2021-06-15 14:07:03 -0500

    Add host repo double logic

--- project/src/test/java/learn/home/data/GuestRepositoryDouble.java
+++ project/src/test/java/learn/home/data/GuestRepositoryDouble.java
@@ -8,10 +8,10 @@ import java.util.List;
 public class GuestRepositoryDouble implements GuestRepository{
 
     private final ArrayList<Guest> guests = new ArrayList<>();
-    private final static Guest GUEST1 = new Guest(1, "Brady", "Baker", "bradybaker82@gmail.com", "573-573-0000", "MO");
-    private final static Guest GUEST2 = new Guest(1, "Free", "Willy", "freewilly82@gmail.com", "573-573-1111", "KY");
-    private final static Guest GUEST3 = new Guest(1, "Mac", "Dog", "macdog@gmail.com", "573-573-2222", "AL");
-    private final static Guest GUEST4 = new Guest(1, "Bugs", "Bunny", "bugsbunny@gmail.com", "573-573-3333", "TX");
+    public final static Guest GUEST1 = new Guest(1, "Brady", "Baker", "bradybaker82@gmail.com", "573-573-0000", "MO");
+    public final static Guest GUEST2 = new Guest(1, "Free", "Willy", "freewilly82@gmail.com", "573-573-1111", "KY");
+    public final static Guest GUEST3 = new Guest(1, "Mac", "Dog", "macdog@gmail.com", "573-573-2222", "AL");
+    public final static Guest GUEST4 = new Guest(1, "Bugs", "Bunny", "bugsbunny@gmail.com", "573-573-3333", "TX");
 
     public GuestRepositoryDouble() {
         guests.add(GUEST1);
--- project/src/test/java/learn/home/data/HostRepositoryDouble.java
+++ project/src/test/java/learn/home/data/HostRepositoryDouble.java
@@ -1,4 +1,33 @@
 package learn.home.data;
 
-public class HostRepositoryDouble {
+import learn.home.models.Host;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HostRepositoryDouble implements HostRepository {
+
+    private final ArrayList<Host> hosts = new ArrayList<>();
+    public final static Host HOST1 = new Host("498604db-b6d6-4599-a503-3d8190fda823", "Smith", "smith@gmail.com", "888-888-0000",
+            "123 Second Ave", "Townie City", "CA", "90210", new BigDecimal(100.00), new BigDecimal(150.00));
+    public final static Host HOST2 = new Host("498604db-b6d6-4599-a503-3d8190fda823", "Smith", "smith@gmail.com", "888-888-0000",
+            "123 Second Ave", "Townie City", "CA", "90210", new BigDecimal(100.00), new BigDecimal(150.00));
+
+    public HostRepositoryDouble() {
+        hosts.add(HOST1);
+        hosts.add(HOST2);
+    }
+
+    @Override
+    public List<Host> findAll() { return new ArrayList<>(hosts); }
+
+    @Override
+    public Host findHostByEmail(String email) {
+        return findAll().stream()
+                .filter(h -> h.getEmail().equalsIgnoreCase(email))
+                .findFirst()
+                .orElse(null);
+    }
+
 }
