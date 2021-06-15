100fdbbd3fa7f711e1fae7e3a192cc9e479ba006
Brady Baker <bradybaker82@gmail.com>
2021-06-15 15:36:31 -0500

    Add basic main menu structure to run app

--- project/src/main/java/learn/home/ui/Controller.java
+++ project/src/main/java/learn/home/ui/Controller.java
@@ -1,4 +1,50 @@
 package learn.home.ui;
 
+import com.sun.tools.javac.Main;
+import learn.home.data.DataAccessException;
+import learn.home.domain.ReservationService;
+
 public class Controller {
+
+    private final ReservationService reservationService;
+    private final View view;
+
+    public Controller(ReservationService reservationService, View view) {
+        this.reservationService = reservationService;
+        this.view = view;
+    }
+
+    public void run() {
+        view.displayHeader("Welcome to this thing that isn't AirBnb at all!");
+        try {
+            runAppLoop();
+        } catch (DataAccessException ex) {
+            view.displayException(ex);
+        }
+        view.displayHeader("Goodbye.");
+    }
+
+    private void runAppLoop() throws DataAccessException {
+        MainMenuOption option;
+        do {
+            option = view.selectMainMenuOption();
+            switch (option) {
+                case EXIT:
+                    view.displayHeader(MainMenuOption.EXIT.getDisplayText());
+                    break;
+                case VIEW_RESERVATION_FOR_HOST:
+                    view.displayHeader(MainMenuOption.VIEW_RESERVATION_FOR_HOST.getDisplayText());
+                    break;
+                case MAKE_A_RESERVATION:
+                    view.displayHeader(MainMenuOption.MAKE_A_RESERVATION.getDisplayText());
+                    break;
+                case EDIT_A_RESERVATION:
+                    view.displayHeader(MainMenuOption.EDIT_A_RESERVATION.getDisplayText());
+                    break;
+                case CANCEL_A_RESERVATION:
+                    view.displayHeader(MainMenuOption.CANCEL_A_RESERVATION.getDisplayText());
+                    break;
+            }
+        } while (option != MainMenuOption.EXIT);
+    }
 }
--- project/src/main/java/learn/home/ui/MainMenuOption.java
+++ project/src/main/java/learn/home/ui/MainMenuOption.java
@@ -1,19 +1,33 @@
 package learn.home.ui;
 
 public enum MainMenuOption {
-    EXIT ("Exit"),
-    VIEW_RESERVATION_FOR_HOST ("View Reservation for Host"),
-    MAKE_A_RESERVATION ("Make a Reservation"),
-    EDIT_A_RESERVATION ("Edit a Reservation"),
-    CANCEL_A_RESERVATION ("Cancel a Reservation");
+    EXIT (0,"Exit"),
+    VIEW_RESERVATION_FOR_HOST (1,"View Reservation for Host"),
+    MAKE_A_RESERVATION (2, "Make a Reservation"),
+    EDIT_A_RESERVATION (3, "Edit a Reservation"),
+    CANCEL_A_RESERVATION (4, "Cancel a Reservation");
 
     private String displayText;
+    private int value;
 
-    private MainMenuOption(String displayText) {
+    private MainMenuOption(int value, String displayText) {
         this.displayText = displayText;
     }
 
     public String getDisplayText() {
         return displayText;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public static MainMenuOption fromValue(int value) {
+        for (MainMenuOption option : MainMenuOption.values()) {
+            if (option.getValue() == value) {
+                return option;
+            }
+        }
+        return EXIT;
     }
 }
